7a1e4fbc577b223877e3ab1ce0d431e52b400779
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-04-25 19:37:19 +0300

    Create posts controller and an empty posts Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController } from './posts/posts.controller';
+import { PostsService } from './posts/posts.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PostsController],
+  providers: [AppService, PostsService],
 })
 export class AppModule {}
---
+++ src/posts/dto/updatePost.dto.ts
@@ -0,0 +1,5 @@
+export default class UpdatePostDto {
+  id: string;
+  content: string;
+  title: string;
+}
