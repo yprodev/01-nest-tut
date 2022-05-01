61d2fbc46b85e87b8002f9ee53b5b866fe476f76
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-01 22:30:45 +0300

    Create Auth Module, Controller and Guard (with other dependencies)

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, HttpCode, Post, Req, UseGuards } from "@nestjs/common";
+
+import { AuthService } from "./auth.service";
+import { RegisterDto } from "./dto/register.dto";
+import { LocalAuthGuard } from "./localAuth.guard";
+import { RequestWithUser } from './requestWithUser.interface'
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registrationData: RegisterDto) {
+    return this.authService.register(registrationData);
+  }
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() request: RequestWithUser) {
+    const user = request.user;
+    
+    //FIXME: Should be fixed
+    user.password = undefined;
+
+    return user;
+  }
+}
