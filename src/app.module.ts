ec17142ea1276b8eeb2176de7561c24ebd99dc8c
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-01 15:10:32 +0300

    Create Posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PostsController } from './posts/posts.controller';
-import { PostsService } from './posts/posts.service';
+import { PostsModule } from './posts/posts.module';
 import { ConfigModule } from '@nestjs/config';
 import * as Joi from '@hapi/joi';
 import { DatabaseModule } from './database/database.module';
@@ -19,9 +18,10 @@ import { DatabaseModule } from './database/database.module';
         PORT: Joi.number()
       })
     }),
-    DatabaseModule
+    DatabaseModule,
+    PostsModule
   ],
-  controllers: [AppController, PostsController],
-  providers: [AppService, PostsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/posts/posts.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { PostsController } from "./posts.controller";
+import { PostsService } from "./posts.service";
+import Post from "./post.entity";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Post])],
+  controllers: [PostsController],
+  providers: [PostsService]
+})
+export class PostsModule {}
