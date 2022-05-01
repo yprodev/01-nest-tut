157450e192312e556cd20a89dbc370d87924417e
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-01 14:38:26 +0300

    Configure the database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,22 @@ import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
 import { ConfigModule } from '@nestjs/config';
 import Joi from '@hapi/joi';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validationSchema: Joi.object({
-      POSTGRES_HOST: Joi.string().required(),
-      POSTGRES_PORT: Joi.number().required(),
-      POSTGRES_USER: Joi.string().required(),
-      POSTGRES_PASSWORD: Joi.string().required(),
-      POSTGRES_DB: Joi.string().required(),
-      PORT: Joi.number()
-    })
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number()
+      })
+    }),
+    DatabaseModule
+  ],
   controllers: [AppController, PostsController],
   providers: [AppService, PostsService],
 })
