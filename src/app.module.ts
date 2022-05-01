af98e0b8149f4898f388ff13cb14ae5de5076478
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-01 14:21:02 +0300

    Use hapi/joi for the environmental variables validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
+import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    validationSchema: Joi.object({
+      POSTGRES_HOST: Joi.string().required(),
+      POSTGRES_PORT: Joi.number().required(),
+      POSTGRES_USER: Joi.string().required(),
+      POSTGRES_PASSWORD: Joi.string().required(),
+      POSTGRES_DB: Joi.string().required(),
+      PORT: Joi.number()
+    })
+  })],
   controllers: [AppController, PostsController],
   providers: [AppService, PostsService],
 })
