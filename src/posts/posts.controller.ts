7a03331f431997f94bee8ad46c0bfaea458c61ca
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-06 20:16:26 +0300

    Create a JWT Guard

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 
 import { PostsService } from './posts.service';
 import CreatePostDto from './dto/createPost.dto';
 import UpdatePostDto from './dto/updatePost.dto';
+import JwtAuthGuard from '../auth/jwtAuth.guard';
 
 @Controller('posts')
 export class PostsController {
@@ -19,11 +20,13 @@ export class PostsController {
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async createPost(@Body() post: CreatePostDto) {
     return this.postsService.createPost(post);
   }
 
   @Put(':id')
+  @UseGuards(JwtAuthGuard)
   async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
     return this.postsService.updatePost(id, post);
   }
