23fd92d020e76907dd88862a1f4fc654ab025570
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-06 20:13:43 +0300

    Add functionality to get the tokens from the cookies and validate them

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { PassportModule } from "@nestjs/passport";
 import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
 import { LocalStrategy } from "./local.strategy";
 
 
@@ -25,7 +26,7 @@ import { LocalStrategy } from "./local.strategy";
       })
     })
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,10 +6,19 @@ import User from "./user.entity";
 import { CreateUserDto } from "./dto/createUser.dto"
 
 const USER_EMAIL_NOT_FOUND_MSG = 'User with this email does not exist';
+const USER_ID_NOT_FOUND_MSG = 'User with this id does not exist';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private usersRepository: Repository<User>) {}
+
+  async getById(id: string) {
+    const user = await this.usersRepository.findOne({ id });
+    
+    if (user) return user;
+
+    throw new HttpException(USER_ID_NOT_FOUND_MSG, HttpStatus.NOT_FOUND)
+  }
 
   async getByEmail(email: string) {
     const user = await this.usersRepository.findOne({ where: { email } });
@@ -18,6 +27,7 @@ export class UsersService {
 
     throw new HttpException(USER_EMAIL_NOT_FOUND_MSG, HttpStatus.NOT_FOUND);
   }
+
   async create(userData: CreateUserDto) {
     const newUser = await this.usersRepository.create(userData);
     await this.usersRepository.save(newUser);
