7aa0d4911a1d77ef078d05e1f37b4c688e905cb6
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-08 20:17:15 +0300

    Apply the filter. Version 1.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
+import { ExceptionLoggerFilter } from './utils/exceptionsLogger.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new ExceptionLoggerFilter(httpAdapter));
+
   app.use(cookieParser());
   await app.listen(3000);
 }
