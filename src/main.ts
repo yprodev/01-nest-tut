3c6a0ada99d4cb6392742585198345e70752df84
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-08 21:20:23 +0300

    Handle optional params ONLY on DTO which is responsible for updatation data. Remove global optional validation.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
+  app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000);
 }
--- src/posts/dto/updatePost.dto.ts
+++ src/posts/dto/updatePost.dto.ts
@@ -1,7 +1,17 @@
+import { IsString, IsNotEmpty, IsUUID, IsOptional } from 'class-validator';
 import { Post } from "../post.interface";
 
 export default class UpdatePostDto implements Post {
+  @IsUUID('4')
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   content: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
   title: string;
 }
