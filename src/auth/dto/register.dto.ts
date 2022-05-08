7eb7e755598a5ddc531f65986d6773ec5a457b3c
Yaroslav Prodanchuk <yaroslav.prodanchuk.external@ryd.one>
2022-05-08 20:56:35 +0300

    Install class-validator and class-transformer to validate the incoming DTOs

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsString, IsNotEmpty, MinLength } from 'class-validator'
 export class RegisterDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(7)
   password: string;
 }
